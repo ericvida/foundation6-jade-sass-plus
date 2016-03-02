4598cb46e9708d5f4037f04b6d6fea32ab18326c
Eric Tirado <mail@erictirado.com>
2016-03-02 11:01:39 -0500

    starter kit update

--- js/app.js
+++ js/app.js
@@ -3,12 +3,11 @@ $(document).foundation();
 
 // See Owl Slider options and add comma before new options.
 // http://www.owlcarousel.owlgraphic.com/docs/api-options.html
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    items: 1,
-    loop: true,
-    autoplay: true,
-  });
+
+$(".owl-carousel").owlCarousel({
+  items: 1,
+  loop: true,
+  autoplay: true,
 });
 
 // Parallax Init
@@ -19,23 +18,22 @@ var parallax = new Parallax(scene);
 // Scrollspeed Smoothing Plugin Init
 // Increase first variable to increase movement amount
 // Increase second variable to increase movement speed
-
 jQuery.scrollSpeed(100, 500);
 
-
-// Wow Scrolling Intro Animations
+// Wow Custom Scrolling Intro Animations
 wow = new WOW(
   {
     animateClass: 'animated',
     offset:       100,
     callback:     function(box) {
     console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
   }
-}
 );
-  wow.init();
-    document.getElementById('moar').onclick = function() {
-    var section = document.createElement('section');
-    section.className = 'section--purple wow fadeInDown';
-    this.parentNode.insertBefore(section, this);
+
+wow.init();
+  document.getElementById('moar').onclick = function() {
+  var section = document.createElement('section');
+  section.className = 'section--purple wow fadeInDown';
+  this.parentNode.insertBefore(section, this);
 };
